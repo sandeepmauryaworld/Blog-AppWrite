58e5879b32c8da80e8163101c656841b84c985c4
Sandeepmauryaworld <85479357+sandeepmauryaworld@users.noreply.github.com>
2023-10-17 21:30:34 +0530

    appwrite env setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+// import "../";
+function App() {
+  // how to access .env file variable
+
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return <h1>sai</h1>;
+}
+
+export default App;
---
+++ src/conf/conf.js
@@ -0,0 +1,11 @@
+// this file for is .env not load some time then this will be working on behind
+
+const conf = {
+  appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+  appwriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
+  appwriteDatabaseId: String(import.meta.env.VITE_DATABASE_ID),
+  appwriteCollectionId: String(import.meta.env.VITE_COLLECTION_ID),
+  appwriteBucketID: String(import.meta.env.VITE_BUCKET_ID),
+};
+
+export default conf;
